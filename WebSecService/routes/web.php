92928ddeaea86eaff9e48bc912dfe4beba5c1b2f
Seif Mohamed Gamaleldin <seif230100575@sut.edu.eg>
2025-10-22 23:28:00 +0300

    Added MiniTest page with supermarket bill using Blade and Bootstrap

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/minitest', function () {
+    $bill = [
+        'customer' => 'John Doe',
+        'date' => '2025-10-22',
+        'items' => [
+            ['name' => 'Apples', 'qty' => 2, 'price' => 3.50],
+            ['name' => 'Bread', 'qty' => 1, 'price' => 2.00],
+            ['name' => 'Milk', 'qty' => 1, 'price' => 1.75],
+            ['name' => 'Rice (1kg)', 'qty' => 2, 'price' => 5.00],
+        ],
+    ];
+
+    return view('minitest', compact('bill'));
 });
